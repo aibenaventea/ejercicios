aeea6c29a45a11da42b30959cd7287fb008051df
Alexander Benavente <aibenaventea@gmail.com>
2022-10-16 22:38:28 -0300

    commit prueba

---
+++ app.js
@@ -0,0 +1,18 @@
+// let obj = {
+//     nombre: "ludwring",
+//     edad: 45,
+//     hola(){
+//         return this.nombre
+//     }
+// }
+// console.log(obj)
+
+
+// console.log( 8 % 3 == 2 )
+
+// console.log( 8 % 3 );
+var x = "0";
+for(var i=1; i<5; i++) {
+    x += i;
+}
+console.log(x);
